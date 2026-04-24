0d0a41bc04e5cbc4ea817db19a5b6c9966f8fa0a
Martin Grolmus <martin@stagro.sk>
2026-04-24 22:04:58 +0200

    fix(opendxp): smoke fixes – config keys, Configuration return type, abstract file names

--- lib/opendxp-deepl-bundle/src/OpendxpDeeplBundle/DependencyInjection/Configuration.php
+++ lib/opendxp-deepl-bundle/src/OpendxpDeeplBundle/DependencyInjection/Configuration.php
@@ -23,7 +23,7 @@ class Configuration implements ConfigurationInterface
     /**
      * {@inheritdoc}
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('opendxp_deepl');
         $rootNode = $treeBuilder->getRootNode();
